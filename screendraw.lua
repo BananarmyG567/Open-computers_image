6c7709067721cfde8fe99408902d9b9157069e33
BananarmyG567 <74381889+BananarmyG567@users.noreply.github.com>
2022-04-15 14:46:04 -0500

    Add files via upload

--- screendraw.lua
+++ screendraw.lua
@@ -1,7 +1,16 @@
 local Serial = require("serialization")
 local component = require("component")
-local Color = require("Color")
+local os = require("os")
+local computer = require("computer")
 local gpu = component.gpu
+
+if computer.getArchitecture and computer.getArchitecture() == "Lua 5.2" then
+    print("Wrong Lua version(take cpu out and crouch right click to switch it to lua 5.3)")
+    os.exit()
+else
+    local func, _ = load("return function(r,g,b) return r<<16|g<<8|b end")
+    _, RGBToInteger = pcall(func)
+end
 
 local arguments_as_a_table = {...}
 
@@ -21,8 +30,8 @@ for y = 1, outS[1][2], 1 do
     for x = 1, outS[1][1], 1 do
 
         if outS[V1] ~= T1 then
-            gpu.setForeground(Color.RGBToInteger(outS[V1][1],outS[V1][2],outS[V1][3]))
-            gpu.setBackground(Color.RGBToInteger(outS[V1][1],outS[V1][2],outS[V1][3]))
+            gpu.setForeground(RGBToInteger(outS[V1][1],outS[V1][2],outS[V1][3]))
+            gpu.setBackground(RGBToInteger(outS[V1][1],outS[V1][2],outS[V1][3]))
             gpu.fill(x,y,1,1," ")
 
         end
